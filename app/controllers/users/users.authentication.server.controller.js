a439c9cb6d9e21dd0f69bd964761e7c38402b63b
Yilun Liu <yil171@ucsd.edu>
2015-11-12 22:46:18 -0800

    Change displayname

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -22,7 +22,7 @@ exports.signup = function(req, res) {
 
 	// Add missing user fields
 	user.provider = 'local';
-	user.displayName = user.firstName + ' ' + user.lastName;
+	user.displayName = user.lastName+user.firstName;
 
 	// Then save the user 
 	user.save(function(err) {
